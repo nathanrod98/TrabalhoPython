d77ae8c1f964d3447655aad3f0cf2379ec7e0b0d
unknown <900155@proway.treina>
2019-11-12 08:36:47 -0300

    aula4

--- aula4/aula4.py
+++ aula4/aula4.py
@@ -3,20 +3,13 @@
 num1 = int(input('digite um numero'))
 num2 = int(input('digite mais um numero'))
 
-resultado = num1 + num2
-print(resultado)
-resultado = num1 - num2
-print(resultado)
-resultado = num1 * num2
-print(resultado)
-resultado = num1 / num2
-print(resultado)
+print(f'Adição: {num1+num2}\nSubtracao: {num1-num2}\nMultiplicacao: {num1*num2}\nDivisao: {num1/num2}')
 
 if num1 > num2:
     print('numero1 é maior')
 elif num1 == num2:
     print("os numeros sao iguais")
 else:
-    print('numero2 é maior')
+    print('"numero2 é maior"')
 
 
